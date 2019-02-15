bfd0b735eed41bf6e0b1a4ad7d21249ceb2d965b
Sandra <sandraonne@gmail.com>
2019-02-15 11:09:08 +0200

    Removing Redundant Code

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,17 +1,19 @@
 describe('Address Book', function() { // hakkame testima aadressiraamatu funktsionaalsusi
-    it('should be able to add a contact', function() { // testime aadressiraamatus kontakti lisamise funktsionaalsust
-        var addressBook = new AddressBook(), // loome AddressBook klassi põhjal objekti addressBook
-            thisContact = new Contact(); // loome Contact klassi põhjal objekti thisContact
+    var addressBook, // koodis korduste vältimiseks defineerime muutujad siin, kust need on kättesaadavad kõikidele aadressraaamtu it meetoditele
+        thisContact;
 
+    beforeEach(function() { // funktsioon, mida käivitatakse enne igat järgmist testi, seega koodis korduste vältimiseks loome klassidest objektid selle funktsiooni käigus
+        addressBook = new AddressBook(); 
+        thisContact = new Contact();
+    });
+
+    it('should be able to add a contact', function() { // testime aadressiraamatus kontakti lisamise funktsionaalsust
         addressBook.addContact(thisContact); // kutsume uue kontakti loomiseks välja addressBook objektil meetodi addContact ja anname sisendiks objekti thisContact
 
         expect(addressBook.getContact(0)).toBe(thisContact); // testimiseks eeldame, et kui võtame aadressraamatust getContact meetodiga esimese kontakti, siis on see sama objekt thisContact
     }); 
 
     it('should be able to delete a contact', function() { // testime kontakti kustutamise funktsionaalsust
-        var addressBook = new AddressBook(),
-            thisContact = new Contact();
-
         addressBook.addContact(thisContact); // lisame aadressiraamatusse kontakti
         addressBook.deleteContact(0); // kutsume aadressiraamatus välja meetodi kontakti kustutamiseks ning anname parameetriks 0 ehk kustutame 1. kontakti
 
